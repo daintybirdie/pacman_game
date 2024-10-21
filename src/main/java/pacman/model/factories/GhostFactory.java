7eafeea9cd953e950db1e0aaeed55806b1c02e0c
Shifali <svel3505@uni.sydney.edu.au>
2024-10-21 13:54:02 +1100

    I think ghost behaviour is only set in the beginning

--- src/main/java/pacman/model/factories/GhostFactory.java
+++ src/main/java/pacman/model/factories/GhostFactory.java
@@ -16,40 +16,37 @@ import java.util.Map;
  * Concrete renderable factory for Ghost objects
  */
 public class GhostFactory implements RenderableFactory {
-
+    // variables to set the corners for each ghost
     private static final int RIGHT_X_POSITION_OF_MAP = 448;
     private static final int TOP_Y_POSITION_OF_MAP = 16 * 3;
     private static final int BOTTOM_Y_POSITION_OF_MAP = 16 * 34;
+    // loading each ghost image
     private static final Image BLINKY_IMAGE = new Image("maze/ghosts/blinky.png");
     private static final Image INKY_IMAGE = new Image("maze/ghosts/inky.png");
     private static final Image CLYDE_IMAGE = new Image("maze/ghosts/clyde.png");
     private static final Image PINKY_IMAGE = new Image("maze/ghosts/pinky.png");
-    private static final Image GHOST_IMAGE = BLINKY_IMAGE;
+    // The renderableTyper variable will be used to dynamically create thr unique ghost type
     private char renderableType;
-    List<Vector2D> targetCorners = Arrays.asList(
-            new Vector2D(0, TOP_Y_POSITION_OF_MAP),
-            new Vector2D(RIGHT_X_POSITION_OF_MAP, TOP_Y_POSITION_OF_MAP),
-            new Vector2D(0, BOTTOM_Y_POSITION_OF_MAP),
-            new Vector2D(RIGHT_X_POSITION_OF_MAP, BOTTOM_Y_POSITION_OF_MAP)
-    );
+    // will hold the specific image a ghost instance the map creates a mapping between the character in map.txt and associated image loaded
     private final Image image;
-
-    static Map<Character, Image> IMAGES = new HashMap<>();
+    private static Map<Character, Image> IMAGES = new HashMap<>();
+    // map will hold the ghost behaviour and allocated corner, for each ghost type
+    private static Map<Character, Map<GhostBehaviour, Vector2D>> GHOSTMAP = new HashMap<>();
 
     static {
         IMAGES.put('b', BLINKY_IMAGE);
         IMAGES.put('s', PINKY_IMAGE);
         IMAGES.put('i', INKY_IMAGE);
         IMAGES.put('c', CLYDE_IMAGE);
+        GHOSTMAP.put('c', Map.of(new ClydeChaseBehaviour(), new Vector2D(0, BOTTOM_Y_POSITION_OF_MAP)));
+        GHOSTMAP.put('s', Map.of(new PinkyChaseBehaviour(), new Vector2D(0, TOP_Y_POSITION_OF_MAP)));
+        GHOSTMAP.put('b', Map.of(new BlinkyChaseBehaviour(), new Vector2D(RIGHT_X_POSITION_OF_MAP, TOP_Y_POSITION_OF_MAP)));
+        GHOSTMAP.put('i', Map.of(new InkyChaseBehaviour(), new Vector2D(RIGHT_X_POSITION_OF_MAP, BOTTOM_Y_POSITION_OF_MAP)));
     }
 
     public GhostFactory(char renderableType) {
         this.renderableType = renderableType;
         this.image = IMAGES.get(renderableType);
-    }
-
-    private int getRandomNumber(int min, int max) {
-        return (int) ((Math.random() * (max - min)) + min);
     }
 
     @Override
@@ -68,33 +65,19 @@ public class GhostFactory implements RenderableFactory {
                     .build();
 
             GhostImpl ghost;
-            Vector2D targetCorner = null;
-            GhostBehaviour chaseBehaviour = null;
-            char name;
+            Vector2D targetCorner;
+            GhostBehaviour chaseBehaviour;
 
-            switch (renderableType) {
-                case 'c':
-                    chaseBehaviour = new ClydeChaseBehaviour();
-                    targetCorner = new Vector2D(0, BOTTOM_Y_POSITION_OF_MAP);
-                    name = renderableType;
-                    break;
-                case 'b':
-                    chaseBehaviour = new BlinkyChaseBehaviour();
-                    targetCorner = new Vector2D(RIGHT_X_POSITION_OF_MAP, TOP_Y_POSITION_OF_MAP);
-                    name = renderableType;
-                    break;
-                case 'i':
-                    chaseBehaviour = new InkyChaseBehaviour();
-                    targetCorner = new Vector2D(RIGHT_X_POSITION_OF_MAP, BOTTOM_Y_POSITION_OF_MAP);
-                    name = renderableType;
-                    break;
-                case 's':
-                    chaseBehaviour = new PinkyChaseBehaviour();
-                    targetCorner = new Vector2D(0, TOP_Y_POSITION_OF_MAP);
-                    name = renderableType;
-                    break;
-                default:
-                    throw new IllegalArgumentException("Unknown ghost type");
+            // Retrieve the behaviour map based on the renderableType
+            Map<GhostBehaviour, Vector2D> behaviourMap = GHOSTMAP.get(renderableType);
+
+            if (behaviourMap != null && !behaviourMap.isEmpty()) {
+                // Get the first entry (assuming only one behavior per key)
+                Map.Entry<GhostBehaviour, Vector2D> entry = behaviourMap.entrySet().iterator().next();
+                chaseBehaviour = entry.getKey();
+                targetCorner = entry.getValue();
+            } else {
+                throw new IllegalArgumentException("Unknown ghost type");
             }
 
             ghost = new GhostImpl(
@@ -103,7 +86,7 @@ public class GhostFactory implements RenderableFactory {
                     kinematicState,
                     GhostMode.SCATTER,
                     targetCorner,
-                    name
+                    renderableType // the renderableType character is the ghost's name for ID purposes
             );
 
             ghost.setGhostBehaviour(chaseBehaviour);
@@ -115,6 +98,4 @@ public class GhostFactory implements RenderableFactory {
                     String.format("Invalid ghost configuration | %s ", e));
         }
     }
-
-
 }
